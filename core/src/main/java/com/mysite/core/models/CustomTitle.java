040b4d58d731b18ed418f5de969866650f1f3ac9
Burkhard Pauli <mail@bpauli.de>
2023-06-13 10:51:46 +0200

    remove unused exporter

--- core/src/main/java/com/mysite/core/models/CustomTitle.java
+++ core/src/main/java/com/mysite/core/models/CustomTitle.java
@@ -1,19 +1,15 @@
 package com.mysite.core.models;
 
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
 
-import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.models.Title;
 
 @Model(adaptables = SlingHttpServletRequest.class,
        resourceType = CustomTitle.RESOURCE_TYPE)
-@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
-          extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class CustomTitle implements Title {
 
     protected static final String RESOURCE_TYPE = "mysite/components/title";
